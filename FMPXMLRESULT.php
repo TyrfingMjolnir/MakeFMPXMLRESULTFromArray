882eefb3e60072e51469a1c1fc3f4dd4ca8d6845
Tyrfing Mjolner <4lphamal3@gmail.com>
2017-02-07 11:33:38 +0100

    Update FMPXMLRESULT.php

--- FMPXMLRESULT.php
+++ FMPXMLRESULT.php
@@ -58,7 +58,7 @@ function MakeFMPXMLRESULTFromArray( $tmp ) {
 }
 /*
 ========================================================================================
-Copyright (c) 2005-2017 Gjermund Gusland Thorsen, released under the MIT License.
+Copyright (c) 2005- Gjermund Gusland Thorsen, released under the MIT License.
 All rights deserved.
 This piece of code comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.
 This piece of text is put at the bottom for you not to be bothered by it.
